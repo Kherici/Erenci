c7dfc4df5fef1bdb3189c46909032859aa1d933d
Kherici <lionmontana3@gmail.com>
2022-05-27 01:13:51 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   env: {
     // @@@@@@@@@ this is only needed for github pages
-      NEXT_PUBLIC_MAIN_APP: 'https://kherici.github.io/Eren',
+      NEXT_PUBLIC_MAIN_APP: 'https://kherici.github.io/Erenci',
     // @@@@@@@@@
     NEXT_PUBLIC_LASTFM_API_KEY: 'ebe2c441c37e356caefe318275757b53',
     NEXT_PUBLIC_Weather_API_KEY: 'e3b952b7c233305e18697b3645934aa6',
     
   },
   // @@@@@@@@@ this is only needed for github pages 
-    assetPrefix: '/Eren',
-    basePath:"/Eren",
+    assetPrefix: '/Erenci',
+    basePath:"/Erenci",
   // @@@@@@@@@
   
  
@@ -27,11 +27,7 @@ module.exports = {
         destination: 'https://github.com/Kherici',
         permanent: true,
       },
-      // {
-      //   source: '/twitter',
-      //   destination: 'https://twitter.com/venolol',
-      //   permanent: true,
-      // },
+      
       {
         source: '/discord',
         destination: 'https://discord.com/users/373006788380852236',
